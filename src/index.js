63b8d2daa19a3ba955c25c8c88c14a96167e69e0
Mutty Faw <106316623+Muttyfaw@users.noreply.github.com>
2023-07-28 14:55:52 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Food</h1>
+    <ol>
+      <li>Jollof</li>
+      <li>Yam</li>
+      <li>Spagethi</li>
+    </ol>
+  </div>,
+  document.getElementById("root"))
